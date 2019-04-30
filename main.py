359bdb81014465e0520a5fa99efd1aec7de9c7fa
Masato Hagiwara <hagisan@gmail.com>
2019-04-29 21:59:16 -0400

    Implement home page

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/health')
-def index():
+def health():
     return "alive"
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     if os.environ['ENVIRONMENT'] == 'production':
