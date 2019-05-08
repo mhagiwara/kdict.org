43dabf14afb65432f9837efd6ff737980d18b0d2
Masato Hagiwara <hagisan@gmail.com>
2019-05-08 13:54:47 -0400

    Implement SERP

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 from elasticsearch import Elasticsearch
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 es = Elasticsearch(['es'])
@@ -16,19 +16,20 @@ def health():
 def home():
     return render_template('index.html')
 
-@app.route('/search/<query>')
-def search(query):
+@app.route('/search')
+def search():
+    query = request.args.get('q')
     hits = []
     res = es.search(index=KEDICT_INDEX, body={'query': {'match': {'word': query}}})
-    hits.extend(res['hits']['hits'])
+    hits.extend(hit['_source'] for hit in res['hits']['hits'])
 
     res = es.search(index=KEDICT_INDEX, body={'query': {'match': {'defs_all': query}}})
-    hits.extend(res['hits']['hits'])
+    hits.extend(hit['_source'] for hit in res['hits']['hits'])
 
     res = es.search(index=KEDICT_INDEX, body={'query': {'match': {'romaja': query}}})
-    hits.extend(res['hits']['hits'])
+    hits.extend(hit['_source'] for hit in res['hits']['hits'])
 
-    return render_template('search.html', hits=hits)
+    return render_template('search.html', hits=hits, query=query)
 
 if __name__ == '__main__':
     if os.environ['ENVIRONMENT'] == 'production':
