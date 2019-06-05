9d5824348c1b8e9d694230a75da1e22f6e39f7b1
Masato Hagiwara <hagisan@gmail.com>
2019-06-05 18:03:57 -0500

    Implement pagination

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import os
 
 from elasticsearch import Elasticsearch
 from flask import Flask, render_template, request, send_from_directory, abort
+import math
 
 app = Flask(__name__)
 es = Elasticsearch(['es'])
 
 KEDICT_INDEX = 'kedict'
-RESULTS_PER_PAGE = 30
+RESULTS_PER_PAGE = 20
 
 @app.route('/health')
 def health():
@@ -20,6 +21,8 @@ def home():
 @app.route('/search')
 def search():
     query = request.args.get('q')
+    current_page = int(request.args.get('p') or '1')     # 1-base
+
     hits = []
     body = {
         'query': {
@@ -28,13 +31,35 @@ def search():
                 'fields': ['word^2', 'defs_all^2', 'romaja'],
             }
         },
+        'from': (current_page - 1) * RESULTS_PER_PAGE,
         'size': RESULTS_PER_PAGE
     }
     res = es.search(index=KEDICT_INDEX, body=body)
-    hits.extend(hit['_source'] for hit in res['hits']['hits'])
-    print(res)
 
-    return render_template('search.html', hits=hits, query=query)
+    # calculate pagination-related info
+    total_hits = res['hits']['total']['value']
+    num_pages = math.ceil(total_hits / RESULTS_PER_PAGE)
+    pagination = []
+    if current_page > 1:
+        pagination.append({'page': 'previous',
+                           'link': '/search?q={}&p={}'.format(query,
+                                                              current_page - 1)})
+    for p in range(max(1, current_page - 4), min(num_pages, current_page + 4)+1):
+        pagination.append({'page': p,
+                           'link': '/search?q={}&p={}'.format(query, p),
+                           'active': p == current_page})
+    if current_page < num_pages:
+        pagination.append({'page': 'next',
+                           'link': '/search?q={}&p={}'.format(query,
+                                                              current_page + 1)})
+
+    hits.extend(hit['_source'] for hit in res['hits']['hits'])
+    print(pagination)
+
+    return render_template('search.html',
+                           hits=hits,
+                           query=query,
+                           pagination=pagination)
 
 @app.route('/word/<word_id>')
 def word(word_id):
