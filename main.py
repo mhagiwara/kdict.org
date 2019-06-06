8d684fbd67df35846bec8eb0263db09578ea8b54
Masato Hagiwara <hagisan@gmail.com>
2019-06-06 17:05:22 -0400

    Show tag link

--- main.py
+++ main.py
@@ -18,6 +18,23 @@ def health():
 def home():
     return render_template('index.html')
 
+def _get_pagination(total_hits, current_page, page_to_link):
+    """Calculate pagination-related info"""
+    num_pages = math.ceil(total_hits / RESULTS_PER_PAGE)
+    pagination = []
+    pagination.append({'page': 'previous',
+                       'link': page_to_link(current_page - 1),
+                       'disabled': current_page == 1})
+    for p in range(max(1, current_page - 4), min(num_pages, current_page + 4)+1):
+        pagination.append({'page': p,
+                           'link': page_to_link(p),
+                           'active': p == current_page})
+    pagination.append({'page': 'next',
+                       'link': page_to_link(current_page + 1),
+                       'disabled': current_page == num_pages})
+
+    return pagination
+
 @app.route('/search')
 def search():
     query = request.args.get('q')
@@ -35,26 +52,13 @@ def search():
         'size': RESULTS_PER_PAGE
     }
     res = es.search(index=KEDICT_INDEX, body=body)
+    hits.extend(hit['_source'] for hit in res['hits']['hits'])
 
     # calculate pagination-related info
     total_hits = res['hits']['total']['value']
-    num_pages = math.ceil(total_hits / RESULTS_PER_PAGE)
-    pagination = []
-    pagination.append({'page': 'previous',
-                       'link': '/search?q={}&p={}'.format(query,
-                                                          current_page - 1),
-                       'disabled': current_page == 1})
-    for p in range(max(1, current_page - 4), min(num_pages, current_page + 4)+1):
-        pagination.append({'page': p,
-                           'link': '/search?q={}&p={}'.format(query, p),
-                           'active': p == current_page})
-    pagination.append({'page': 'next',
-                       'link': '/search?q={}&p={}'.format(query,
-                                                          current_page + 1),
-                       'disabled': current_page == num_pages})
-
-    hits.extend(hit['_source'] for hit in res['hits']['hits'])
-    print(pagination)
+    def _page_to_link(p):
+        return '/search?q={}&p={}'.format(query, p)
+    pagination = _get_pagination(total_hits, current_page, _page_to_link)
 
     return render_template('search.html',
                            hits=hits,
@@ -73,6 +77,9 @@ def word(word_id):
 
 @app.route('/tag/<tag>')
 def tag(tag):
+    title = {'topik1': 'TOPIK 1 Vocabulary List'}[tag]
+    current_page = int(request.args.get('p') or '1')  # 1-base
+
     hits = []
     body = {
         'query': {
@@ -80,13 +87,21 @@ def tag(tag):
                 'tags': tag
             }
         },
+        'from': (current_page - 1) * RESULTS_PER_PAGE,
         'size': RESULTS_PER_PAGE
     }
     res = es.search(index=KEDICT_INDEX, body=body)
     hits.extend(hit['_source'] for hit in res['hits']['hits'])
-    print(res)
+    # calculate pagination-related info
+    total_hits = res['hits']['total']['value']
+    def _page_to_link(p):
+        return '/tag/{}?p={}'.format(tag, p)
+    pagination = _get_pagination(total_hits, current_page, _page_to_link)
 
-    return render_template('search.html', hits=hits)
+    return render_template('search.html',
+                           title=title,
+                           hits=hits,
+                           pagination=pagination)
 
 @app.route('/css/<path:path>')
 def send_js(path):
